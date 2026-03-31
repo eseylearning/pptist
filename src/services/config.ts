70c8f087ec87f61f65983c93200a1a0d5b201ef2
zdd <291856414@qq.com>
2026-03-31 17:50:00 +0800

    feat:加密修改

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,34 +1,63 @@
 import axios from "axios";
 import message from "@/utils/message";
+import { Encrypt, Decrypt, generateRandomKey, rsaEncrypt } from "@/utils/crypto";
 
 const instance = axios.create({ timeout: 1000 * 300 });
 
 function getParamFromQueryString(paramName: string) {
-  // 获取当前页面的查询字符串
   const queryString = window.location.search;
-
-  // 使用 getParamFromString 函数获取指定参数的值
   return getParamFromString(queryString, paramName);
 }
 function getParamFromString(queryString: string, paramName: string) {
-  // 如果传入的字符串为空或参数名为空，则返回 null
   if (!queryString || !paramName) {
     return null;
   }
-
-  // 使用 URLSearchParams 解析查询字符串
   const params = new URLSearchParams(queryString);
-
-  // 获取指定参数的值
   return params.get(paramName);
 }
 
-instance.interceptors.request.use((config) => {
-  // 获取 token
+instance.interceptors.request.use((config: any) => {
+  const { url } = config;
+
+  if (url !== "/api/f/files/upload/single") {
+    const randomKey = generateRandomKey();
+    const encryptedKey = rsaEncrypt(randomKey);
+    config._aesKey = randomKey;
+
+    if (url && url.split("?")[1]) {
+      const obj: any = {};
+      const formatUrl = url.split("?")[1].split("&");
+      for (let i = 0; i < formatUrl.length; i++) {
+        const item = formatUrl[i].split("=");
+        obj[item[0]] = item[1];
+      }
+      config.url = url.split("?")[0];
+      config.params = { encode: Encrypt(obj, randomKey), encryptKey: encryptedKey };
+    } else {
+      if (config.params !== undefined && Object.keys(config.params).length !== 0) {
+        for (const key in config.params) {
+          if (config.params[key].length === 0) {
+            delete config.params[key];
+          }
+        }
+        config.params = { encode: Encrypt(config.params, randomKey), encryptKey: encryptedKey };
+      }
+    }
+
+    if (config.data !== undefined && !(config.data instanceof FormData) && Object.keys(config.data).length !== 0) {
+      config.data = { encode: Encrypt(config.data, randomKey), encryptKey: encryptedKey };
+    }
+
+    if (!config.params?.encryptKey && !config.data?.encryptKey) {
+      config.params = { ...(config.params || {}), encryptKey: encryptedKey };
+    }
+  }
+
   const token = getParamFromQueryString("token");
   config.headers["source-type"] = "web";
   config.headers["Language-Type"] = "1";
-  config.headers["encode"] = "";
+  config.headers["Cache-Control"] = "no-cache";
+  config.headers["encode"] = "1";
   if (token) {
     config.headers["X-Csrf-Token"] = token;
   }
@@ -38,7 +67,16 @@ instance.interceptors.request.use((config) => {
 instance.interceptors.response.use(
   (response) => {
     if (response.status >= 200 && response.status < 400) {
-      return Promise.resolve(response.data);
+      const aesKey = (response.config as any)._aesKey;
+      let data: any;
+      if (response.request?.responseType === "blob") {
+        data = response.data;
+      } else {
+        data = response.data.code
+          ? response.data
+          : JSON.parse(Decrypt(response.data, aesKey));
+      }
+      return Promise.resolve(data);
     }
 
     message.error("未知的请求错误！");
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,6 +1,18 @@
 import CryptoJS from 'crypto-js'
+import JSEncrypt from 'jsencrypt'
 
 const CRYPTO_KEY = 'pptist'
+
+const RSA_PUBLIC_KEY = `MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmAPN4W5KvokYMNUyt311
+OdPA3lq59El7ETpbNpxSBG5lWpEKw4KjiOIc6N5GRXbPwJWCrWI9RTGdh71Nhykt
+5dUjE6kGastnjNhiq92d//5UbbXH1goz8cPYmJ05QgbFDbhT4ilMft9MpUpYIPF+
+O4ZfLQyffTn2Lt+SBPihepOb2ZT4jpeoojAF3veWGH7cG8PN9CXe6MtEhs5JsPi1
+jtbf4pgs4Slkxj1E9cw2bg9E54ozwraJjnP4X73SILboyotG2uh7K6Ejrzi4lbJr
+prSGoMd7DJpyZqJ6sywnQqw/saJ8SJDMn74tItFKckTN1xvktjSxxpNmDqB5jW0C
+7QIDAQAB`
+
+const FIXED_KEY = CryptoJS.enc.Utf8.parse('1234567887654321')
+const iv = CryptoJS.enc.Utf8.parse('ABCDEF1234123412')
 
 /**
  * 加密
@@ -17,4 +29,45 @@ export const encrypt = (msg: string) => {
 export const decrypt = (ciphertext: string) => {
   const bytes = CryptoJS.AES.decrypt(ciphertext, CRYPTO_KEY)
   return bytes.toString(CryptoJS.enc.Utf8)
+}
+
+export function generateRandomKey(length = 16) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return result
+}
+
+export function rsaEncrypt(str: string) {
+  const encryptor = new JSEncrypt()
+  encryptor.setPublicKey(RSA_PUBLIC_KEY)
+  return encryptor.encrypt(str)
+}
+
+export function Encrypt(str: any, customKey?: string) {
+  let type = typeof str
+  if (type === 'object') {
+    str = JSON.stringify(str)
+  } else if (type !== 'string') {
+    str += ''
+  }
+  const aesKey = customKey ? CryptoJS.enc.Utf8.parse(customKey) : FIXED_KEY
+  const encrypted = CryptoJS.AES.encrypt(str, aesKey, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  })
+  return encrypted.toString()
+}
+
+export function Decrypt(word: string, customKey?: string) {
+  const aesKey = customKey ? CryptoJS.enc.Utf8.parse(customKey) : FIXED_KEY
+  const decrypted = CryptoJS.AES.decrypt(word, aesKey, {
+    iv: iv,
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+  })
+  return decrypted.toString(CryptoJS.enc.Utf8)
 }
